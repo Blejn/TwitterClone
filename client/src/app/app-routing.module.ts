31afd7817d15e09940eb2a57fa1b3d475df94771
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2023-03-18 09:34:51 +0100

    main routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full', //przekierowanie całościowe
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      // enableTracing: true, //do opcji debugowania,
+      // initialNavigation: 'disabled',
+      //czy router ma sie na starcie uruchomić
+      // useHash: true,
+      //zeby recznie zmieniac routing bez paska
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
