d29cbde99b0b3546ebf1180f195719e876a6407d
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2023-04-14 16:23:34 +0200

    routing for friends

--- client/src/app/pages/friends/friends-routing.module.ts
+++ client/src/app/pages/friends/friends-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FriendsComponent } from './friends.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 const routes: Routes = [
   {
     path: 'friends',
     component: FriendsComponent,
+  },
+  {
+    path: 'friend/:id',
+    component: UserDetailsComponent,
   },
 ];
 
