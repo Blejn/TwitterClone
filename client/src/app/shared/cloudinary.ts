1cf80523586a0546672446e928d84fb5f79bddff
Sebastian Mazur <102309928+Blejn@users.noreply.github.com>
2023-06-03 08:27:31 +0200

    All functionalities of app added

---
+++ client/src/app/shared/cloudinary.ts
@@ -0,0 +1,11 @@
+import { Cloudinary } from '@cloudinary/url-gen';
+import { environment } from 'src/environments/environment';
+
+const cloudinary = new Cloudinary({
+  cloud: {
+    cloudName: environment.CLOUD_NAME,
+    apiKey: environment.API_KEY_CLOUD,
+    apiSecret: environment.API_SECRET_CLOUD,
+  },
+});
+export const cloudinaryConfiguration = new Clo();
