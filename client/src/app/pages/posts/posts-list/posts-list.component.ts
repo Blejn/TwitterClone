4f73cef9a3be972fa967c2e904d216165b97e1a8
Sebastian <sebastian.mazur.p@gmail.com>
2023-09-05 17:08:24 +0200

    translatio features in post-list.component

--- client/src/app/pages/posts/posts-list/posts-list.component.ts
+++ client/src/app/pages/posts/posts-list/posts-list.component.ts
@@ -1,51 +1,67 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable, combineLatest } from 'rxjs';
 import { Post } from 'src/app/interfaces/Post';
+import { AppState } from 'src/app/interfaces/states/AppState';
+import { CookieServiceService } from 'src/app/services/cookie-service.service';
 import { PostService } from 'src/app/services/post.service';
+import * as PostsAcions from '../store/actions/post.actions';
+import {
+  allPostslengthtSelector,
+  currentLengthtSelector,
+  isLoadingSelector,
+  postSelector,
+} from '../store/selectors/post.selector';
+import { PostResult } from './../../../interfaces/states/PostsStateInterface';
 
 @Component({
   selector: 'posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class PostsListComponent implements OnInit {
-  posts: Post[] = [];
-  public state: any = {
-    data: '',
-    page: 1,
-    limit: 3,
-  };
+  isLoading$!: Observable<boolean>;
+  length!: number;
+  posts!: Post[];
+  postsResult$!: PostResult;
+  areMorePosts!: boolean;
+  combined$ = combineLatest(
+    this.store.pipe(select(currentLengthtSelector)),
+    this.store.pipe(select(allPostslengthtSelector))
+  );
 
-  constructor(public postService: PostService) {}
+  constructor(
+    public postService: PostService,
+    public cookieService: CookieServiceService,
+    private store: Store<AppState>
+  ) {
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+  }
 
   ngOnInit(): void {
     this.getPosts();
   }
 
   getPosts() {
-    this.postService
-      .getPosts(this.state.data, this.state.page, this.state.limit)
-      .subscribe({
-        next: (response: any) =>
-          response.results.map((post: Post) => this.posts.push(post)),
-        error: (error) =>
-          alert(
-            'Wystąpił błąd podczas pobierania postów. Spróbuj ponownie później.'
-          ),
-        complete: () => console.log('wykonane'),
-      });
+    this.store.dispatch(PostsAcions.getPosts());
+    this.combined$.subscribe(([a, b]) =>
+      a === b ? this.areMorePosts === false : this.areMorePosts === true
+    );
+    this.store
+      .pipe(select(postSelector))
+      .subscribe((results) => (this.posts = [...results]));
   }
 
   loadMorePosts() {
-    this.state.page += 1;
-    this.postService
-      .getPosts(this.state.data, this.state.page, this.state.limit)
-      .subscribe({
-        next: (response: any) =>
-          response.results.map((post: Post) => this.posts.push(post)),
-        error: () =>
-          alert(
-            'Wystąpił błąd podczas pobierania postów. Spróbuj ponownie później.'
-          ),
-      });
+    this.store.dispatch(PostsAcions.getMorePosts());
   }
 }
