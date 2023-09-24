aebca69776581fd44399852e653f82f9632331c9
Sebastian <sebastian.mazur.p@gmail.com>
2023-09-24 17:48:28 +0200

    add state of authorization

--- client/src/app/interfaces/states/AppState.ts
+++ client/src/app/interfaces/states/AppState.ts
@@ -1,7 +1,9 @@
+import { AuthStateInterface } from './AuthStateInterface';
 import { LanguageStateInterface } from './LanguageStateInterface';
 import { PostsStateInterface } from './PostsStateInterface';
 
 export interface AppState {
   posts: PostsStateInterface;
   language: LanguageStateInterface;
+  auth: AuthStateInterface;
 }
